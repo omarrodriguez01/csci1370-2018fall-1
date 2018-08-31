a762c73d48d1a138e3615f469878e3a9fc8873cb
JJ Lumagbas <jjlumagbas@gmail.com>
2018-08-31 10:36:52 -0500

    Add Lab 1

---
+++ lab/lab1.rkt
@@ -0,0 +1,96 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname lab1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+; Section #:
+; Student ID 1:
+; Student ID 2:
+
+; Purpose: To start using the DrRacket Integrated Development Environment (IDE),
+; and to practice writing Beginning Student Language (BSL) Racket simple and
+; compound expressions of several data types within that environment.
+
+; Due: by end of lab today. Submit whatever you have by the end of lab, even if incomplete.
+
+
+; PART 1
+; Under each comment below, write an expression that meets the comment's description
+
+; 1.1 simple expression of type number
+
+
+; 1.2 simple expression of type string
+
+
+; 1.3 simple expression of type boolean
+
+
+; 1.4 simple expression of type image
+
+
+; Remember: compound expressions involve operations on other expressions
+
+; 1.5 compound expression of type number
+
+
+; 1.6 compound expression of type string
+
+
+; 1.7 compound expression of type boolean
+
+
+; 1.8 compound expression of type image
+
+
+; PART 2
+
+; The following are currently NOT "correct" Racket expressions --
+; they do NOT follow Racket's syntax rules.
+
+; Correct each (with a minimum of alteration) so that the result is
+; an expression that DOES follow Racket's syntax rules, and type those
+; corrected expressions in your Definitions window (each starting on its own line).
+;
+; (Note: sometimes there is more than one reasonable way to correct each of these!)
+
+; 2.1
+(13.13)
+
+; 2.2
+circle ("green" 55)
+
+; 2.3
+(+ (48) (/ 15 3)) (5 * 8)))
+
+
+; PART 3
+
+; Look over pages 1 and 2 of the handout, "Some DrRacket Tidbits"
+; Choose at least three different image or image-related functions
+; from this handout that you will try out. Then, after a blank line,
+; write at least three compound expressions, each using at least one
+; different image or image-related function from this handout.
+
+; 3.1
+; compound expression of type image 1
+
+; 3.2
+; compound expression of type image 2
+
+; 3.3
+; compound expression of type image 1
+
+
+; PART 4
+
+; Write a SINGLE compound expression in DrRacket that meets all of the following requirements:
+; - It should result in an image.
+; - It should use at least THREE different image or image-related operations from the 2htdp/image
+;    module/teachpack. (More is fine!)
+; – They can be image operations from the "Some DrRacket Tidbits" handout, OR
+;    they can be image operations (from the 2htdp/image teachpack) that you find by
+;    exploring DrRacket's Help feature, described a bit at the end of
+;    the "Some DrRacket Tidbits" handout.
+
