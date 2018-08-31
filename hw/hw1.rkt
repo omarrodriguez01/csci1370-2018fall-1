e6e19f23dae4f315419e3f9fb87de9916b705068
JJ Lumagbas <jjlumagbas@gmail.com>
2018-08-31 17:45:06 -0500

    Add HW 1

---
+++ hw/hw1.rkt
@@ -0,0 +1,28 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname hw1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Getting started: click that button labelled "Raw", 
+; and copy-paste the text from this file to your DrRacket Editor pane
+
+(require 2htdp/image)
+(require 2htdp/universe)
+
+; Section #: 1
+; Student ID:
+
+; We are going to make a class "quilt" of 200-pixels-wide, 200-pixels-high images,
+; with each class member designing one of the 200-pixel-by-200-pixel images.
+;
+; After a blank line, write one or more expressions that meet the following requirements:
+; - the final resulting image should be the result of at least THREE different image or image-
+;   related operations from the 2htdp/image teachpack. (More is fine!)
+; - the final resulting image should be an expression of type image that is precisely
+;   200 pixels wide and 200 pixels high (use image-width and image-height to check)
+
+
+
+
+; SUBMISSION: 
+; - Save this file as hw1.rkt
+; - Submit to Blackboard: Assignments (in the left-hand sidebar) > HW 1
+; - Submit as many times as you like! The last one will be considered the final submission
