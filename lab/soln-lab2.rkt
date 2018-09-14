6a6640cbe4126e7e09bbfb4e0d4ec60bfaa44f1c
JJ Lumagbas <jjlumagbas@gmail.com>
2018-09-14 14:22:07 -0500

    Add solutions for labs 1-3

---
+++ lab/soln-lab2.rkt
@@ -0,0 +1,51 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname soln-lab2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; For each:
+; DESIGN RECIPE
+; 1. Data definitions (NOT REQUIRED)
+; 2. Signature (2 points), purpose (2 points), stub (2 points)
+; 3. Examples (Minimum of 2 examples: 2 points)
+; 4. Template (2 points)
+; 5. Function definition (5 points)
+; 6. Test and debug (0 points)
+; TOTAL 10 points/problem
+
+
+; Getting started: click that button labelled "Raw", 
+; and copy-paste the text from this file to your DrRacket Editor pane
+
+; Student ID 1:
+; Student ID 2:
+
+; Purpose: To practice designing functions using the design recipe.
+
+; Due: by end of lab today. Submit whatever you have by the end of lab, even if incomplete.
+
+; Refer to lab2-recipe-example.rkt for reference
+
+; General instructions:
+; - Follow the design recipe! The objective is to learn the steps
+;
+; - Do not write over any intermediate steps (stub and template),
+;   instead, comment them out and leave them in your file
+;   (refer to lab2-recipe-example.rkt to see what I mean)
+;
+; - For the Examples step, write at least 2 different check-expect expressions
+
+
+; Part 1: Design a function named say-bye that expects
+;   someone's name, and produces a string containing
+;   "Goodbye, " followed by their name
+
+
+
+; Part 2: Design a function named avg-trio that expects three numbers, 
+;   and produces the average of those three numbers.
+
+
+
+
+; SUBMISSION: 
+; - Save this file as lab2.rkt
+; - EACH student in the pair should submit (the same file) to Blackboard: Exercises (in the left-hand sidebar) > Lab 2
